3e17df08d887eb0de235cf4ab5e505b84e955959
Justin Green <justingreen.business@gmail.com>
2023-01-30 17:31:48 +1100

    Tutorial02 (PhysicsScene) : Completed the Physiscs Scene update and Circle2Circle functions, as well as the destructor

--- Physics/PhysicsScene.cpp
+++ Physics/PhysicsScene.cpp
@@ -1,12 +1,24 @@
 #include "PhysicsScene.h"
 
 #include "PhysicsObject.h"
+#include "Circle.h"
 
 PhysicsScene::PhysicsScene()
 {
 	m_timeStep = 0.01f;
 	m_gravity = glm::vec2(0);
 
+}
+
+PhysicsScene::~PhysicsScene()
+{
+	for (auto pActor : m_actors)
+	{
+		if (pActor != nullptr)
+		{
+			delete pActor;
+		}
+	}
 }
 
 void PhysicsScene::AddActor(PhysicsObject* _actor)
@@ -32,6 +44,24 @@ void PhysicsScene::Update(float _dt)
 
 		accumulatedTime -= m_timeStep;
 	}
+
+	// check for collisions (ideally you'd want to have some sort of 
+	// scene management in place)
+	int actorCount = m_actors.size();
+
+	// need to check for collisions against all objects except this one.
+	for (int outer = 0; outer < actorCount - 1;outer++)
+	{
+		for (int inner = outer + 1; inner < actorCount;inner++)
+		{
+			PhysicsObject* object1 = m_actors[outer];
+			PhysicsObject* object2 = m_actors[inner];
+
+			// for now we can assume both shapes are Circles, 
+			// since that is all we’ve implemented for now.
+			Circle2Circle(object1, object2);
+		}
+	}
 }
 
 void PhysicsScene::Draw()
@@ -41,6 +71,19 @@ void PhysicsScene::Draw()
 		pActor->Draw(1);
 	}
 
+}
+
+bool PhysicsScene::Circle2Circle(PhysicsObject* _obj1, PhysicsObject* _obj2)
+{
+	Circle* sphere1 = dynamic_cast<Circle*>(_obj1);
+	Circle* sphere2 = dynamic_cast<Circle*>(_obj2);
+
+	if (sphere1 != nullptr && sphere2 != nullptr)
+	{
+		// If distance of two spheres is less then sum of both sphere radius
+		return glm::distance(sphere1->GetPosition(), sphere2->GetPosition()) <=
+			(sphere1->GetRadius() + sphere2->GetRadius());
+	}
 }
 
 
